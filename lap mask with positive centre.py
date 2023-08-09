1ab86278e94d02feaa88f42e0a78660d411d533f
Madan Reddy <113412527+imadan07@users.noreply.github.com>
2023-08-09 07:05:52 +0530

    Update lap mask with positive centre.py

--- lap mask with positive centre.py
+++ lap mask with positive centre.py
@@ -1,27 +1,15 @@
 import cv2
 import numpy as np
-
-# Load the image
-image_path = 'path_to_your_image.jpg'
+image_path = 'spidy.png.jpg'
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
-# Apply Gaussian blur to reduce noise before applying Laplacian filter
 blurred_image = cv2.GaussianBlur(image, (3, 3), 0)
-
-# Define the Laplacian kernel with a positive center coefficient
 laplacian_kernel = np.array([
     [ 0,  1,  0],
     [ 1, -4,  1],
     [ 0,  1,  0]
 ])
-
-# Apply the Laplacian kernel with a positive center coefficient
 opened_image = cv2.filter2D(blurred_image, cv2.CV_64F, laplacian_kernel)
-
-# Display the original image and the opened image
 cv2.imshow('Original Image', image)
 cv2.imshow('Opened Image (Positive Laplacian)', opened_image)
-
-# Wait for a key press and then close the windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
